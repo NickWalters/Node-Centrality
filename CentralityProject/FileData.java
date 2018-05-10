f9c7fce5bf9a2131cb505fd08c8af422b5ba98ac
JamesCaldon <james.caldon@outlook.com>
2018-05-10 14:22:31 +0800

    Updated FileData

--- CentralityProject/FileData.java
+++ CentralityProject/FileData.java
@@ -1,7 +1,5 @@
 import java.util.*;
-import CITS2200.*;
 import java.io.*;
-import java.nio.*;
 /**
  * This class reads the text files containing the nodes, and prints output
  *
@@ -9,100 +7,66 @@ import java.nio.*;
  * @version 9 May 2018
  */
 public class FileData
-{
-    // instance variables - replace the example below with your own
+{    
     private String filename;
-    private int numNodes;
-    private ArrayList<Integer> nodeNumbers;
-    private HashMap<Integer, Integer> nodeNumHash;
-    
     /**
      * initialise data to work with
      * use this data for algorithms
      */
-    public FileData()
+    public FileData() 
     {
-          nodeNumbers = new ArrayList<Integer>();
-          nodeNumHash = new HashMap<Integer, Integer>();
-          readFile(1);
-          numNodes = nodeNumbers.size();
+
     }
-    
     /**
      * input 1, to read 42833.edges.txt
      * input 2, to read 78813.edges.txt
      * Doing this allows distinction between which file you are using
      */
-    public void readFile(int whichFile){
+    public ArrayList<String> readFile(int whichFile){
         if(whichFile == 1){
-            String filename = "428333.edges.txt";
-            readFileHelper(filename);
+            filename = "428333.edges.txt";
+            return readFileHelper(filename);
         }
         else if(whichFile == 2){
-            String filename = "78813.edges.txt";
-            readFileHelper(filename);
+            filename = "78813.edges.txt";
+            return readFileHelper(filename);
         }
         else{
             System.out.println("--ERROR--, please select from the two files");
-            
+            return null;
         }
     }
     
-    private void readFileHelper(String filename){
-        int i = 0;
+    private ArrayList<String> readFileHelper(String filename){
+
+        ArrayList<String> nodes = new ArrayList<String>();
         try(BufferedReader reader = new BufferedReader(new FileReader(filename)))
         {
-            String lines;
-            int firstVertex;
-            int secondVertex;
-            String[] twoNodes;
-            
-            while((lines = reader.readLine()) != null){
-                twoNodes = lines.split(" ");
-                //convert string representation to Integer
-                firstVertex = Integer.parseInt(twoNodes[0]);
-                secondVertex = Integer.parseInt(twoNodes[0]);
-                if(!nodeNumbers.contains(firstVertex)){
-                    nodeNumbers.add(firstVertex);
-                    nodeNumHash.put(firstVertex, i);
-                    i++;
-                }
-                if(!nodeNumbers.contains(secondVertex)){
-                    nodeNumbers.add(secondVertex);
-                    nodeNumHash.put(secondVertex, i);
-                    i++;
-                }
+            String line;
+            while((line = reader.readLine()) != null) {
+                nodes.add(line);
             }
             reader.close();
         }
         catch(IOException e){
             System.out.println("--Error, Cant read file-- " + e.getMessage());
         }
-          
+        return nodes;
     }
     
-    /**
-     * must call method readFile first!
-     * this method prints out all the UNIQUE/DISTINCT nodes provided from the txt file
-     */
-    public void printNodes(){
-        String str = " | ";
-        for(int node: nodeNumbers){
-            str += node + " | ";
-        }
-        System.out.println(str);
-    }
-    
-    public int[] getAjMatrix(){
-        return null;
-    }
-    
-    public String getFileName(){
+    public String getFileName() {
         return filename;
     }
-    
+    /*
+    public int[][] getAjMatrix(){
+
+        return null;
+    }
+    */
+    /*
     public ArrayList<Integer> getUniqueNodes(){
         return nodeNumbers;
     }
+    */
 }
 
