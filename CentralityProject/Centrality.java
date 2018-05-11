c2e78a07010a6989c6de8633f5fd0c58aab96ec1
Nick Walters <nicho.walters@gmail.com>
2018-05-11 15:17:08 +0800

    Betweeness
    ok

--- CentralityProject/Centrality.java
+++ CentralityProject/Centrality.java
@@ -23,31 +23,31 @@ public class Centrality
      * @param allNodesUnique, a list containing info of all distinct nodes
      */
     public void getDegreeCentrality(ArrayList<String> nodes){
-        HashMap<Integer, Integer> nodesMap = new HashMap<>();
-        
+        HashMap<Integer, Integer> nodesIndex = new HashMap<>();
+        HashMap<Integer, Integer> indexNodes = new HashMap<>();
         for(String line: nodes){
             // get two nodes per line, put in array
             String[] bothNodes = line.split(" ");
             // if hashmap contains these nodes already, then increment its edgeValue
             // if not in hashmap, then add Node and its edge count is 1
-            if(!nodesMap.containsKey(bothNodes[0])){
-                nodesMap.put(Integer.parseInt(bothNodes[0]), 1);
+            if(!nodesIndex.containsKey(bothNodes[0])){
+                nodesIndex.put(Integer.parseInt(bothNodes[0]), 1);
             }
             else{
-                int numEdges = nodesMap.get(bothNodes[0]);
-                nodesMap.put(Integer.parseInt(bothNodes[0]), numEdges++);
+                int numEdges = nodesIndex.get(bothNodes[0]);
+                nodesIndex.put(Integer.parseInt(bothNodes[0]), numEdges++);
             }
             
-            if(!nodesMap.containsKey(bothNodes[1])){
-                nodesMap.put(Integer.parseInt(bothNodes[1]), 1);
+            if(!nodesIndex.containsKey(bothNodes[1])){
+                nodesIndex.put(Integer.parseInt(bothNodes[1]), 1);
             }
             else{
-                int numEdges = nodesMap.get(bothNodes[1]);
-                nodesMap.put(Integer.parseInt(bothNodes[1]), numEdges++);
+                int numEdges = nodesIndex.get(bothNodes[1]);
+                nodesIndex.put(Integer.parseInt(bothNodes[1]), numEdges++);
             }
         }
         //print all the nodes, with their associated values
-        System.out.println(nodesMap.entrySet());
+        System.out.println(nodesIndex.entrySet());
     }
     
     
@@ -55,17 +55,95 @@ public class Centrality
         return null;
     }
     
-    
-    public Node[] getBetweenessCentrality(int[][] ajMatrix, int numNodes){
-        //analyse the adjacency matrix of edges
-        //each row of the ajacency matrix gives enough information for that nodes specific BC
-        // do this for every row, and sum up all the BC's
-        for(int i=0; i<numNodes; i++){
-            for(int j=0; i<numNodes; i++){
-                
+    /**
+     * Brandes algorithm is the most efficient algorithm for betweeness Centrality
+     * this runs in O(nm) time, compared to all other algorithms which require O(n^3) time for unweighted graphs
+     */
+    public float[] getBetweenessCentrality(int[][] edgeMatrix){
+       int numNodes = edgeMatrix.length;
+       float[] betweenessArray = new float[numNodes];
+        // Brandes algorithm O(EV^2) for UNWEIGHTED graphs: 
+        // Brandes algorithm : https://people.csail.mit.edu/jshun/6886-s18/papers/BrandesBC.pdf
+        
+        // assign the shortest paths list to use later on. Corresponds to P on paper
+        ArrayList paths[] = new ArrayList[numNodes];
+        // create a sigma list according to paper (σ)
+        float sigma[] = new float[numNodes];
+        // create a delta list according to paper (δ)
+        float[] delta = new float[numNodes];
+        // holds the distance for each iteration of the paths
+        int distances[];
+        // assign an empty queue
+        Queue<Integer> queue;
+        
+        // for each s in V
+        // beginning with the starting node, for all Vertex V which is an element of the graph G do:
+        for(int startingNode = 0; startingNode < numNodes; startingNode++)
+        {
+            Stack<Integer> stack = new Stack<Integer>();
+            distances = new int[numNodes];
+            for(int i = 0; i<numNodes; i++)
+            {
+                paths[i] = new ArrayList(); // create an array inside of an array, to store the different sequences shortest paths
+                sigma[i] = 0;
+                distances[i] = -1;
+            }
+            sigma[startingNode] = 1;
+            distances[startingNode] = 0;
+            
+            queue = new ArrayDeque<Integer>();
+            queue.add(startingNode);
+            int v = 0; // current Node/Vertex
+            // while Q not empty do:
+            while(!queue.isEmpty())
+            {
+                // dequeue v from Q and push to S
+                v = queue.remove();
+                stack.push(v);
+                // for each neighbour w of v/currentVertex do:
+                for(int currentNeighbor = 0; currentNeighbor< numNodes; currentNeighbor++)
+                {
+                    if(edgeMatrix[v][currentNeighbor] == 1)
+                    {
+                        if(distances[v]<0)
+                        {
+                            queue.add(currentNeighbor);
+                            distances[currentNeighbor] = distances[v]+1;
+                        }
+                        if(distances[currentNeighbor] == distances[v]+1)
+                        {
+                            sigma[currentNeighbor] += sigma[v];
+                            paths[currentNeighbor].add(v);
+                        }
+                    }
+                }
+            }
+            
+            for(int i = 0; i< numNodes; i++)
+            {
+                //δ[v] = 0, for all vertex thats an element of Graph
+                delta[i] = 0;
+            }
+            
+            //While Stack is not empty do:
+            while(!stack.isEmpty())
+            {
+                // pop one by one
+                v = stack.pop();
+                // for each vertex in P/Paths, delta[w] = delta[w] + (sigma[w] / sigma[v]) * (1+ delta[v])
+                java.util.Iterator<Integer> pathIterator = paths[v].iterator();
+                int w; // w is the neighbour
+                while(pathIterator.hasNext())
+                {
+                    w = pathIterator.next();
+                    delta[w] = delta[w] + ((sigma[w])/ (sigma[v]))*(1+ delta[v]);
+                }
+                if(v != startingNode){
+                    betweenessArray[v] += (float)delta[v];
+                }
             }
         }
-        return null;
+        return betweenessArray;
     }
     
     
--- CentralityProject/FileData.java
+++ CentralityProject/FileData.java
@@ -10,35 +10,42 @@ import java.io.*;
 public class FileData
 {    
     private String filename;
+    private ArrayList<String> nodes;
+    private int[][] ajMatrix;
     /**
      * initialise data to work with
      * use this data for algorithms
      */
-    public FileData() 
+    public FileData(int whichFile) throws Exception
     {
-
+        ArrayList<String> nodes = readFile(whichFile);
+        ajMatrix = generateAjMatrix(filename);
     }
     /**
      * input 1, to read 42833.edges.txt
      * input 2, to read 78813.edges.txt
      * Doing this allows distinction between which file you are using
      */
-    public ArrayList<String> readFile(int whichFile){
-        if(whichFile == 1){
+    public ArrayList<String> readFile(int whichFile) throws Exception{
+        try{
+            if(whichFile == 1){
             filename = "428333.edges.txt";
             return readFileHelper(filename);
-        }
-        else if(whichFile == 2){
+            }   
+            if(whichFile == 2){
             filename = "78813.edges.txt";
             return readFileHelper(filename);
+            }
         }
-        else{
-            System.out.println("--ERROR--, please select from the two files");
-            return null;
+        catch(Exception e){
+            System.out.println("--ERROR--, please select from the two files 1 or 2");
+            throw new Exception("cannot find" + e.getMessage());
         }
+        return null;
     }
     
-    private ArrayList<String> readFileHelper(String filename){
+    
+    private ArrayList<String> readFileHelper(String filename) throws Exception{
 
         ArrayList<String> nodes = new ArrayList<String>();
         try(BufferedReader reader = new BufferedReader(new FileReader(filename)))
@@ -49,10 +56,26 @@ public class FileData
             }
             reader.close();
         }
-        catch(IOException e){
+        catch(Exception e){
             System.out.println("--Error, Cant read file-- " + e.getMessage());
+            throw new FileNotFoundException("Not found");
         }
         return nodes;
+    }
+    
+    // not finished
+    private int[][] generateAjMatrix(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        Scanner fileReader = new Scanner(file);
+        fileReader.useDelimiter(" |\\n");
+        int numVertex = 0;
+        int[][] edgeMatrix = new int[numVertex][numVertex];
+        while(fileReader.hasNext()){
+            int x = Integer.parseInt(fileReader.next());
+            int y = Integer.parseInt(fileReader.next());
+        }
+        
+        return null;
     }
     
     public String getFileName() {
