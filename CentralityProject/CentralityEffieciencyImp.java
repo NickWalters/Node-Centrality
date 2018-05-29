79a29cbcf20a84b8dcbed1a7b85b3e05ac097eb0
Nick Walters <nicho.walters@gmail.com>
2018-05-29 22:16:30 +0800

    merge branch

---
+++ CentralityProject/CentralityEffieciencyImp.java
@@ -0,0 +1,232 @@
+import java.util.*;
+/**
+ * This is the big boy class. all the centrality work is calculated here
+ * But! dont make it too big! you can use helper classes too if nessessary!
+ *
+ * @author Nicholas Walters and James Caldon 22226341
+ * @version 9 May 2018 (last change by Nick)
+ */
+public class CentralityEffieciencyImp
+{
+
+    private ArrayList<ArrayList<Integer>> degreeCentralities = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> katzCentralities = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> closenessCentralities = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> betweennessCentralities = new ArrayList<>();
+    
+
+    
+    public CentralityEffieciencyImp(Graph2 g)
+    {
+    	calculateCentralities(g);
+    }
+	/**
+	 * @author James
+	 * holds reference to the vertex and its weight
+	 */
+    
+	public class Node {
+		public final Integer v;
+		public final Float w;
+		Node (Integer vertex, Float weight) {
+			v = vertex;
+			w = weight;
+		}
+	}
+	
+	/**
+	 * @author James
+	 * Comparator for two nodes, used by the priority queue
+	 * the highest priority is the lower value
+	 */
+	public class NodeComparator implements Comparator<Node>{
+
+		@Override
+		public int compare(Node o1, Node o2) {
+			if (o1.w < o2.w) return 1;
+			else if (o1.w > o2.w) return -1;
+			return 0;
+		}	
+		
+	}
+    
+    
+    private void calculateCentralities(Graph2 g) {
+	    int numNodes = g.getNumberOfVertices();
+        float catz[] = new float[numNodes];
+        float alpha = 0.5f;	
+	    float[] betweenessCentralities = new float[numNodes];
+	    int[][] weightsOfShortestPaths = new int[numNodes][];
+		ArrayList<PriorityQueue<Node>> pqList = new ArrayList<PriorityQueue<Node>>();
+		ArrayList<HashSet<Integer>> adjList = g.getAdjList();
+
+        // Brandes algorithm O(EV^2) for UNWEIGHTED graphs: 
+        // Brandes algorithm : https://people.csail.mit.edu/jshun/6886-s18/papers/BrandesBC.pdf
+        
+        Stack<Integer> stack;
+
+        // create a sigma list according to paper (Ã�Æ’)
+        float sigma[] = new float[numNodes];
+        // create a delta list according to paper (ÃŽÂ´)
+        float delta[] = new float[numNodes];
+        // holds the distance for each iteration of the paths
+        int distances[];
+        // assign an empty queue
+        Queue<Integer> queue;
+        boolean visited[] = new boolean[numNodes];
+        int component[] = new int[numNodes];
+        // for each s in V
+        // beginning with the starting node, for all Vertex V which is an element of the graph G do:
+        visited[0] = true; 
+	    float[] closeness = new float[g.getNumberOfVertices()];
+	    pqList.add(new PriorityQueue<>(new NodeComparator()));
+	    
+	    int cN = 1;
+        for(int startingNode = 0; startingNode < numNodes; startingNode++)
+        {
+        	System.out.println(pqList.size() + " " + cN + " " + startingNode);
+            // assign the shortest paths list to use later on. Corresponds to P on paper
+            ArrayList<ArrayList<Integer>> paths = new ArrayList<ArrayList<Integer>>();
+            stack = new Stack<Integer>();
+            distances = new int[numNodes];
+            if (!visited[startingNode]) {
+            	for(int i = 0; i < numNodes; i++) {
+            		if (!visited[i]) {
+            			component[i] = cN;
+            		}
+            	}
+            	pqList.add(new PriorityQueue<>(new NodeComparator()));
+            	cN++;
+            }
+            for(int i = 0; i<numNodes; i++)
+            {
+                paths.add(new ArrayList<Integer>()); // create an array inside of an array, to store the different sequences shortest paths
+                sigma[i] = 0;
+                distances[i] = -1;
+            }
+            sigma[startingNode] = 1;
+            distances[startingNode] = 0;
+            
+            queue = new ArrayDeque<Integer>();
+            queue.add(startingNode);
+            visited[startingNode] = true;
+            int v; // current Node/Vertex
+            // while Q not empty do:
+            while(!queue.isEmpty())
+            {
+                // dequeue v from Q and push to S
+                v = queue.remove();
+                stack.push(v);
+                HashSet<Integer> adjacent = adjList.get(v);
+                // for each neighbour w of v/currentVertex do:
+                for (Integer currentNeighbor : adjacent) {
+                    if(distances[currentNeighbor] < 0)
+                    {
+                        queue.add(currentNeighbor);
+                        visited[currentNeighbor] = true;
+                        distances[currentNeighbor] = distances[v]+1;
+                    }
+                    if(distances[currentNeighbor] == distances[v]+1)
+                    {
+                        sigma[currentNeighbor] += sigma[v];
+                        paths.get(currentNeighbor).add(v);
+                    }
+                    weightsOfShortestPaths[v] = distances; 
+                    
+                }
+                closeness[startingNode] += distances[v];
+            }
+
+            
+            for(int i = 0; i< numNodes; i++)
+            {
+
+                //ÃŽÂ´[v] = 0, for all vertex thats an element of Graph
+                delta[i] = 0f;
+                if (visited[i]) {
+                	catz[startingNode] = catz[startingNode] + (float) (adjList.get(i).size()*Math.pow(alpha,weightsOfShortestPaths[startingNode][i]));
+                }
+
+            }
+
+            
+            //While Stack is not empty do:
+            while(!stack.isEmpty())
+            {
+                // pop one by one
+                v = stack.pop();
+                // for each vertex in P/Paths, delta[w] = delta[w] + (sigma[w] / sigma[v]) * (1+ delta[v])
+                java.util.Iterator<Integer> pathIterator = paths.get(v).iterator();
+                int w; // w is the neighbour
+                while(pathIterator.hasNext())
+                {
+                    w = pathIterator.next();
+                    delta[w] += ((sigma[w])/(sigma[v]))*(1f + delta[v]);
+                }
+                if(v != startingNode){
+                    betweenessCentralities[v] += delta[v];
+
+                }
+                
+            }
+
+            closeness[startingNode] = 1/closeness[startingNode];
+      
+        }
+    	for (int i = 0; i < adjList.size(); i++) {
+			pqList.get(component[i]).add(new Node(i, (float) adjList.get(i).size()));
+		}
+    	
+    	findTopFive(pqList, degreeCentralities, g);
+        
+        for (int k = 0; k < numNodes; k++) {
+            pqList.get(component[k]).add(new Node(k, closeness[k]));
+		}
+
+        findTopFive(pqList, closenessCentralities, g);
+        for (int k = 0; k < numNodes; k++) {
+            pqList.get(component[k]).add(new Node(k, betweenessCentralities[k]));
+
+		}
+        
+        findTopFive(pqList, betweennessCentralities, g);
+
+
+        
+	
+        for (int i = 0; i < numNodes; i++) {
+			for (int j = 0; j < numNodes; j++) {
+				//Should this be here or in line 147
+				//catz[i] = catz[i] + (float) (adjList.get(j).size()*Math.pow(alpha,weightsOfShortestPaths[i][j]));
+			}
+			pqList.get(component[i]).add(new Node(i, catz[i]));
+		}
+        
+        findTopFive(pqList, katzCentralities, g);
+    }
+    
+    private void findTopFive(ArrayList<PriorityQueue<Node>> pqList, ArrayList<ArrayList<Integer>> aL, Graph2 g) {
+        for (int i = 0; i < pqList.size(); i++) {
+        	int count = 0;
+        	aL.add(new ArrayList<>());
+			while (!pqList.get(i).isEmpty() && count < 5) {
+				aL.get(i).add(g.getVertex(pqList.get(i).poll().v));
+				count++;
+			}
+            pqList.get(i).clear();
+		}
+    }
+    
+    public ArrayList<ArrayList<Integer>> returnKatzCentrality(){
+        return katzCentralities;
+    }
+    public ArrayList<ArrayList<Integer>> returnClosenessCentrality(){
+        return closenessCentralities;
+    }
+    public ArrayList<ArrayList<Integer>> returnBetweenessCentrality(){
+        return betweennessCentralities;
+    }
+    public ArrayList<ArrayList<Integer>> returnDegreeCentrality(){
+        return degreeCentralities;
+    }
+}
--- CentralityProject/Graph.java
+++ CentralityProject/Graph.java
@@ -4,8 +4,11 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 
 /**
@@ -19,12 +22,16 @@ import java.util.Queue;
 public class Graph {
 
 	private int[][] adjMatrix;
-	private ArrayList<Integer[][]> adjMatrixList = new ArrayList<Integer[][]>();
+	private ArrayList<int[][]> adjMatrixList = new ArrayList<int[][]>();
 	private ArrayList<ArrayList<HashSet<Integer>>> adjListList = new ArrayList<ArrayList<HashSet<Integer>>>();
 	private ArrayList<int[]> edges = new ArrayList<>();
 	private String filename;
 	private ArrayList<HashSet<Integer>> adjList = new ArrayList<>();
 	private Hashtable<Integer,Integer> vertices = new Hashtable<Integer, Integer>();
+	private Hashtable<Integer,Integer> verticesIndex = new Hashtable<Integer, Integer>();
+	private ArrayList<Hashtable<Integer, Integer>> verticesIndexList = new ArrayList<Hashtable<Integer, Integer>>();
+	private ArrayList<HashSet<Integer>> verticesList = new ArrayList<>();
+	private int numberComponents = 1;
 	/**
 	 * @throws FileNotFoundException 
 	 * 
@@ -35,6 +42,7 @@ public class Graph {
 		
 		generateAdjMatrix();
 		findComponents();
+		generateAdjMatrixForComponents();
 	}
 	
 	private void readEdgeList(String filename) throws FileNotFoundException {
@@ -56,13 +64,16 @@ public class Graph {
         }
 	}
 	private void findComponents() {
-		
+
 		int vertex = 0;
 		int size = vertices.size();
 		boolean foundAllComponents;
+		numberComponents = 0;
+		int[] visited = new int[size];
 		do {
-			foundAllComponents = false;
-			int[] visited = new int[size];
+			verticesList.add(new HashSet<Integer>());
+			verticesList.get(numberComponents).add(vertex);
+			foundAllComponents = true;
 	    	visited[vertex] = 1;
 	    	Queue<Integer> q = new LinkedList<Integer>();
 	
@@ -74,46 +85,52 @@ public class Graph {
 	    		for (Integer i : adjacent) {
 	    			if(visited[i] == 0) {
 						visited[i] = 1;
+						verticesList.get(numberComponents).add(i);
 						q.add(i);
 	    			}
 				}
 	    	}
-	    for (int i = 0; i < visited.length; i++) {
-			if (visited[i] == 0) {
-				foundAllComponents = false;
-				break; 
+		    for (int i = 0; i < visited.length; i++) {
+				if (visited[i] == 0) {
+					foundAllComponents = false;
+					vertex = i;
+					break; 
+				}
 			}
-		}
-		} while (foundAllComponents);
+			numberComponents++;
+		} while (!foundAllComponents);
 	}
 	public int getNumberOfVertices() {
 		return adjMatrix.length;
 	}
 	
+	public int getNumberOfVertices(int componentNumber) {
+		return adjListList.get(componentNumber).size();
+	}
+	
 	private void generateAdjMatrix() {
 		// Logic replicated from https://www.reddit.com/r/learnprogramming/comments/4ktlwx/making_an_adjacency_matrix_from_an_edge_list_text/
-		Hashtable<Integer,Integer> vertices = new Hashtable<Integer, Integer>();
     	int count = 0;
     	for (int[] line: edges) {
     		int[] vertex = line;
-    		if (!vertices.containsKey(vertex[0])) {
-    			vertices.put(vertex[0], count);
+    		if (!verticesIndex.containsKey(vertex[0])) {
+    			verticesIndex.put(vertex[0], count);
     			this.vertices.put(count, vertex[0]);
     			adjList.add(count, new HashSet<>());
     			count++;
     		}
-    		if (!vertices.containsKey(vertex[1])) {
-    			vertices.put(vertex[1], count);
+    		if (!verticesIndex.containsKey(vertex[1])) {
+    			verticesIndex.put(vertex[1], count);
     			this.vertices.put(count, vertex[1]);
     			adjList.add(count, new HashSet<>());
     			count++;
     		}
 		}
-    	adjMatrix = new int[vertices.size()][vertices.size()];
+    	adjMatrix = new int[verticesIndex.size()][verticesIndex.size()];
     	for (int i = 0; i < edges.size(); i++) {
     		
-    		int node1 = vertices.get(edges.get(i)[0]);
-    		int node2 = vertices.get(edges.get(i)[1]);
+    		int node1 = verticesIndex.get(edges.get(i)[0]);
+    		int node2 = verticesIndex.get(edges.get(i)[1]);
 			adjMatrix[node1][node2] = 1;
 			adjMatrix[node2][node1] = 1;
 			HashSet<Integer> connect1 = adjList.get(node1);
@@ -126,6 +143,57 @@ public class Graph {
 			}
 		}
 
+    }
+	
+	private void generateAdjMatrixForComponents() {
+		ArrayList<Hashtable<Integer,Integer>> vertices = new ArrayList<Hashtable<Integer,Integer>>();
+		for (int i = 0; i < verticesList.size(); i++) {
+			vertices.add(new Hashtable<Integer, Integer>());
+			adjListList.add(new ArrayList<>());
+			verticesIndexList.add(new Hashtable<Integer, Integer>());
+		}
+    	int counts[] = new int[verticesList.size()];
+    	for (int[] line: edges) {
+    		int[] vertex = line;
+    		
+    		for (int i = 0; i < verticesList.size(); i++) {
+	    		if (!vertices.get(i).containsKey(vertex[0]) && verticesList.get(i).contains(verticesIndex.get(vertex[0]))) {
+	    			
+	    			vertices.get(i).put(vertex[0], counts[i]);
+	    			adjListList.get(i).add(counts[i], new HashSet<>());
+	    			verticesIndexList.get(i).put(counts[i], vertex[0]);
+	    			counts[i]++;
+
+	    		}
+	    		if (!vertices.get(i).containsKey(vertex[1]) && verticesList.get(i).contains(verticesIndex.get(vertex[1]))) {
+	    			
+	    			vertices.get(i).put(vertex[1], counts[i]);
+	    			adjListList.get(i).add(counts[i], new HashSet<>());
+	    			verticesIndexList.get(i).put(counts[i], vertex[1]);
+	    			counts[i]++;
+	    			
+	    		}
+    		}
+		}
+    	for (int cN = 0; cN < verticesList.size(); cN++) {
+	    	adjMatrixList.add(new int[verticesList.get(cN).size()][verticesList.get(cN).size()]);
+	    	for (int i = 0; i < edges.size(); i++) {
+	    		if (vertices.get(cN).containsKey(edges.get(i)[0])) {
+		    		int node1 = vertices.get(cN).get(edges.get(i)[0]);
+		    		int node2 = vertices.get(cN).get(edges.get(i)[1]);
+					adjMatrixList.get(cN)[node1][node2] = 1;
+					adjMatrixList.get(cN)[node2][node1] = 1;
+					HashSet<Integer> connect1 = adjListList.get(cN).get(node1);
+					HashSet<Integer> connect2 = adjListList.get(cN).get(node2);
+					if (!connect1.contains(node2)) {
+		    			adjListList.get(cN).get(node1).add(node2);
+					}
+					if (!connect2.contains(node1)) {
+		    			adjListList.get(cN).get(node2).add(node1);
+					}
+	    		}
+			}
+    	}
     }
 	
 	@Override
@@ -141,30 +209,51 @@ public class Graph {
 	}
 
 	
-    public int[][] getAdjMatrix(){
-    	int[][] adjMatrixClone = new int[adjMatrix.length][];
-    	for (int i = 0; i < adjMatrix.length; i++) {
-			adjMatrixClone[i] = adjMatrix[i].clone(); 
-		}
-        return adjMatrixClone;
+    public int[][] getAdjMatrix(int componentNumber){
+    	if (numberComponents == 1) {
+	    	int[][] adjMatrixClone = new int[adjMatrix.length][];
+	    	for (int i = 0; i < adjMatrix.length; i++) {
+				adjMatrixClone[i] = adjMatrix[i].clone(); 
+			}
+	        return adjMatrixClone;
+    	} else {
+    		int[][] adjMatrixClone = new int[adjMatrixList.get(componentNumber).length][];
+	    	for (int i = 0; i < adjMatrixList.get(componentNumber).length; i++) {
+				adjMatrixClone[i] = adjMatrixList.get(componentNumber)[i].clone(); 
+			}
+	        return adjMatrixClone;
+    	}
+
     }
     
     
-    public int getVertex(int index) {
-        return vertices.get(index);
+    public int getVertex(int index, int componentNumber) {
+        return verticesIndexList.get(componentNumber).get(index);
     }
     
-    public ArrayList<HashSet<Integer>> getAdjList(){
-    	ArrayList<HashSet<Integer>> adjListClone = new ArrayList<>();
-    	for (int i = 0; i < adjList.size(); i++) {
-			adjListClone.add(new HashSet<Integer>(adjList.get(i)));
-		}
-        return adjListClone;
+    public ArrayList<HashSet<Integer>> getAdjList(int componentNumber){
+    	if (numberComponents == 1) {
+	    	ArrayList<HashSet<Integer>> adjListClone = new ArrayList<>();
+	    	for (int i = 0; i < adjList.size(); i++) {
+				adjListClone.add(new HashSet<Integer>(adjList.get(i)));
+			}
+	        return adjListClone;
+    	} else {
+	    	ArrayList<HashSet<Integer>> adjListClone = new ArrayList<>();
+	    	for (int i = 0; i < adjListList.get(componentNumber).size(); i++) {
+				adjListClone.add(new HashSet<Integer>(adjListList.get(componentNumber).get(i)));
+			}
+	        return adjListClone;
+    	}
     }
     //For Testing
     public ArrayList<int[]> getEdges() {
 
     	return edges;
+    }
+    
+    public int getNumberComponents() {
+        return numberComponents;
     }
     
     public String getFilname(){
